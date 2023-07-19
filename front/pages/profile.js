4be2b97c52ef24e4815e9453cbcdd42d08d1c0a4
se-yoo <se.yoo857@gmail.com>
2023-07-19 19:12:36 +0900

    프로젝트 생성 및 화면 작업

---
+++ front/pages/profile.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Head from "next/head";
+
+import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+
+const Profile = () => {
+  const followerList = [
+    { nickname: "제로초" },
+    { nickname: "순둥도리노" },
+    { nickname: "노드버드" },
+  ];
+  const followingList = [
+    { nickname: "제로초" },
+    { nickname: "순둥도리노" },
+    { nickname: "노드버드" },
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>내 프로필 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
+    </>
+  );
+};
+
+export default Profile;
---
+++ front/pages/signup.js
@@ -0,0 +1,111 @@
+import React, { useState, useCallback } from "react";
+import Head from "next/head";
+import useInput from "../hooks/useInput";
+import { Button, Checkbox, Form, Input } from "antd";
+import styled from "styled-components";
+
+import AppLayout from "../components/AppLayout";
+
+const ErrorMessage = styled.div`
+  color: red;
+`;
+
+const Signup = () => {
+  const [id, onChangeId] = useInput("");
+  const [nickname, onChangeNickname] = useInput("");
+  const [password, onChangePassword] = useInput("");
+
+  const [passwordCheck, setPasswordCheck] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setPasswordError(e.target.value !== password);
+    },
+    [password],
+  );
+
+  const [term, setTerm] = useState(false);
+  const [termError, setTermError] = useState(false);
+  const onChangeTerm = useCallback((e) => {
+    setTerm(e.target.checked);
+    setTermError(!e.target.checked);
+  }, []);
+
+  const onSubmit = useCallback(() => {
+    if (password !== passwordCheck) {
+      return setPasswordError(true);
+    }
+    if (!term) {
+      return setTermError(true);
+    }
+    alert("완료");
+  }, [password, passwordCheck, term]);
+
+  return (
+    <>
+      <Head>
+        <title>회원가입 | NodeBird</title>
+      </Head>
+      <AppLayout>
+        <Form onFinish={onSubmit}>
+          <div>
+            <label htmlFor="user-id">아이디</label>
+            <br />
+            <Input name="user-id" value={id} required onChange={onChangeId} />
+          </div>
+          <div>
+            <label htmlFor="user-nickname">닉네임</label>
+            <br />
+            <Input
+              name="user-nickname"
+              value={nickname}
+              required
+              onChange={onChangeNickname}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password">비밀번호</label>
+            <br />
+            <Input
+              name="user-password"
+              type="password"
+              value={password}
+              required
+              onChange={onChangePassword}
+            />
+          </div>
+          <div>
+            <label htmlFor="user-password-check">비밀번호 확인</label>
+            <br />
+            <Input
+              name="user-password-check"
+              type="password"
+              value={passwordCheck}
+              required
+              onChange={onChangePasswordCheck}
+            />
+            {passwordError && (
+              <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>
+            )}
+          </div>
+          <div>
+            <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>
+              개인정보 이용에 동의합니다.
+            </Checkbox>
+            {termError && (
+              <ErrorMessage>약관에 동의하셔야 합니다.</ErrorMessage>
+            )}
+          </div>
+          <div style={{ marginTop: 10 }}>
+            <Button type="primary" htmlType="submit">
+              가입하기
+            </Button>
+          </div>
+        </Form>
+      </AppLayout>
+    </>
+  );
+};
+
+export default Signup;
